781b9c73f6b515f6fc90c17cbc6bff3965b626be
Your Name <varunshahi829@gmail.com>
2025-07-28 02:45:20 +0530

    Fix: Update cookie secure setting for production environment

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -55,11 +55,11 @@ const user=await User.create({
 
 const token=await genToken(user._id)
 
-res.cookie("token",token,{
+   res.cookie("token",token,{
     httpOnly:true,
     maxAge:7*24*60*60*1000,
     sameSite:"Strict",
-    secure:false
+    secure: process.env.NODE_ENV === "production"
    })
 
    return res.status(201).json(user)
@@ -88,7 +88,7 @@ export const login=async (req,res)=>{
      httpOnly:true,
      maxAge:7*24*60*60*1000,
      sameSite:"Strict",
-     secure:false
+     secure: process.env.NODE_ENV === "production"
     })
  
     return res.status(200).json(user)
